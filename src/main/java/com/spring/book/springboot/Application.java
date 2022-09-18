409a32937d427dd335496d6b261dbc967db51d30
Spring boot start <yumkl123@gmail.com>
2022-09-18 17:01:19 +0900

    재설정

--- src/main/java/com/spring/book/springboot/Application.java
+++ src/main/java/com/spring/book/springboot/Application.java
@@ -2,11 +2,14 @@ package com.spring.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
-// @EnableJpaAuditing가 삭제됨 // JPA Auditing 활성화
+// @EnableJpaAuditing 삭제됨 // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
